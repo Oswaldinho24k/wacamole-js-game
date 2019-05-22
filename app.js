208c03c365297248f3e053a522b8d04efa6e20e4
oswaldo  martinez <oswaldinho24k@MacBook-Pro-de-oswaldo.local>
2019-05-21 21:44:23 -0500

    finished with buttons

--- app.js
+++ app.js
@@ -1,7 +1,61 @@
-//game variables
+//variables
+const topos = document.querySelectorAll('.topo')
+const button = document.querySelector('#start')
+const restartBtn = document.querySelector('#restart')
+const timeText = document.querySelector('.time')
+const scoreText = document.querySelector('.score')
 
-//dom variables
+let interval;
+let vel = 1000
+let time = 29000
+let score = 0
 
-//listeners
 
-//functions
+
+
+//show and hide the topo
+const startGame=()=>{
+    button.disabled = true
+    interval = setInterval(()=>{
+        const randomTopo = Math.floor(Math.random()*topos.length)
+        topos[randomTopo].classList.add('show')
+        setTimeout(()=>{
+            topos[randomTopo].classList.remove('show')
+        }, vel)
+
+        timeText.innerText = `Time: ${time/1000}` 
+        time-=1000
+    },1000)
+
+    setTimeout(gameOver, 30000)
+}
+
+const gameOver=()=>{
+    clearInterval(interval)
+    restartBtn.disabled = false
+
+}
+
+const restartGame=()=>{
+    button.disabled = false
+    restartBtn.disabled=true
+    score = 0
+    scoreText.innerText = `Score: ${score}`
+    time = 29000
+    timeText.innerText = `Time: ${time/1000}` 
+}
+const hitTopo = (event) =>{
+    event.target.classList.remove('show')
+    score++
+    scoreText.innerText = `Score: ${score}`
+}
+
+
+
+//controls
+button.addEventListener('click', startGame)
+topos.forEach((topo, idx)=>{
+  topo.addEventListener('click',hitTopo)  
+})
+restartBtn.addEventListener('click', restartGame)
+
